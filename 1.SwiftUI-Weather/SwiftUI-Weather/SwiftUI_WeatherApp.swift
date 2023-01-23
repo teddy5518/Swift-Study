2e07b3aa8ccf319f1fbed40a0e2c4deb6f76cce4
TED <teddy5518@hotmail.com>
2023-01-23 20:52:44 +0900

    Tutorial #1

---
+++ 1.SwiftUI-Weather/SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by TEDDY on 1/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
