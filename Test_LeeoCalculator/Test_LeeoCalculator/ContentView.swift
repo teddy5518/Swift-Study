65405379c7ff32f1b77327572b6efd91661fb892
TED <teddy5518@hotmail.com>
2023-01-27 08:00:00 +0000

    Tutorial #3

--- Test_LeeoCalculator/Test_LeeoCalculator/ContentView.swift
+++ Test_LeeoCalculator/Test_LeeoCalculator/ContentView.swift
@@ -10,10 +10,33 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          HStack {
+            Text("7")
+            Text("8")
+            Text("9")
+            Text("X")
+          }
+          
+          HStack {
+            Text("4")
+            Text("5")
+            Text("6")
+            Text("-")
+          }
+          
+          HStack {
+            Text("1")
+            Text("2")
+            Text("3")
+            Text("+")
+          }
+          
+          HStack {
+            Text("0")
+            Text("0")
+            Text(".")
+            Text("=")
+          }
         }
         .padding()
     }
